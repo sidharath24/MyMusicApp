cd3a8443e28f58e1e563feedcd874943aff3e62d
Sidharath <sid.aggarwal.31@gmail.com>
2019-07-03 18:47:01 -0400

    Added layouts for individual artist songs

--- app/src/main/java/com/example/mymusicapp/HomeFragment.java
+++ app/src/main/java/com/example/mymusicapp/HomeFragment.java
@@ -1,9 +1,13 @@
 package com.example.mymusicapp;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -15,5 +19,13 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
     }
 }
